e379c278fb3409672449c3abff3f85238a6b9c45
P. Antony <philemon.antony@students.fhnw.ch>
2013-11-25 10:29:20 +0100

    Some work on filter selection

--- source/src/view/FilterSelection.java
+++ source/src/view/FilterSelection.java
@@ -1,14 +1,41 @@
 package view;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 
 public class FilterSelection extends JPanel{
 
 	/*
-	 * GUI part for selecitng the filter
+	 * GUI part for selecitng the filterFilterSelectionFilterSelection
 	 */
+	
+	// Filters
+	JButton btnFilterRedColorAnalyzer;
+	JButton btnFilterBlueColorAnalyzer;
+	
 	public FilterSelection(){
+		
+		btnFilterRedColorAnalyzer = new JButton("Red");
+		btnFilterRedColorAnalyzer.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		
+		
+		btnFilterBlueColorAnalyzer = new JButton("Blue");
+		
+		
+		this.add(new JButton());
+		
 		
 	}
 
