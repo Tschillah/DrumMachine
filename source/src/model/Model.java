cd6273b6cf2f9d315258f6c35f8a803ed7f5453f
n0daft <eric.neher@gmx.ch>
2014-01-06 08:45:52 +0100

    soundmanager introduced

--- source/src/model/Model.java
+++ source/src/model/Model.java
@@ -10,15 +10,14 @@ import java.io.IOException;
 import java.util.LinkedList;
 
 import javax.imageio.ImageIO;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 import strategies.ColorAnalyzer;
 import strategies.IImageAnalyzer;
 import framework.INotifyable;
 
-
-
-public class Model implements PropertyChangeListener{
-
+public class Model implements PropertyChangeListener {
 
 	PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
 	private static Model instance = null;
@@ -28,26 +27,29 @@ public class Model implements PropertyChangeListener{
 	private int loopLength;
 	private IImageAnalyzer imageAnalyzer;
 
-
-
 	final int LINECOUNT = 6;
 	final int COLCOUNT = 16;
 	final int BLOCKCOUNT = LINECOUNT * COLCOUNT;
 	private BufferedImage image;
 	private BufferedImage imageBlocks[][] = new BufferedImage[COLCOUNT][LINECOUNT];
 	private boolean buttonStates[][] = new boolean[COLCOUNT][LINECOUNT];
+	private String[] sampleLines = { "clave.wav", "perc7.wav", "perc3.wav",
+			"snare1.wav", "snare2.wav", "snare3.wav" };
 
 	private LinkedList<INotifyable> listeners = new LinkedList<INotifyable>();
+	private SoundManager sou;
 
+	public Model() {
 
-	public Model(){
+		sou = new SoundManager();
+
 		try {
 			image = ImageIO.read(new File("res/farben.jpg"));
-		//	image = ImageIO.read(new File("res/hongkong.jpg"));
+			// image = ImageIO.read(new File("res/hongkong.jpg"));
 
 			imageAnalyzer = new ColorAnalyzer(Color.RED);
 
-		//	imageAnalyzer = new GrayScaleAnalyzer();
+			// imageAnalyzer = new GrayScaleAnalyzer();
 			divideImage();
 			analyzeImage();
 		} catch (IOException e) {
@@ -66,7 +68,6 @@ public class Model implements PropertyChangeListener{
 		analyzeImage();
 	}
 
-	
 	/*
 	 * Divides the image into smaller parts
 	 */
@@ -74,21 +75,23 @@ public class Model implements PropertyChangeListener{
 
 		for (int x = 0; x < COLCOUNT; x++) {
 			for (int y = 0; y < LINECOUNT; y++) {
-				
-				imageBlocks[x][y] = image.getSubimage(getBlockWidth()*x, getBlockHeight()*y, getBlockWidth(), getBlockHeight());
-			
+
+				imageBlocks[x][y] = image
+						.getSubimage(getBlockWidth() * x, getBlockHeight() * y,
+								getBlockWidth(), getBlockHeight());
+
 			}
 		}
 
 		System.out.println(image);
 
 	}
-	
-	public void toggleButton(int x, int y){
+
+	public void toggleButton(int x, int y) {
 		buttonStates[x][y] = !buttonStates[x][y];
 		notifyListeners();
 	}
-	
+
 	/*
 	 * Analayzed all image parts with the analyzer which is currently set.
 	 */
@@ -97,56 +100,52 @@ public class Model implements PropertyChangeListener{
 		// send image blocks to analyzer
 		for (int x = 0; x < COLCOUNT; x++) {
 			for (int y = 0; y < LINECOUNT; y++) {
-		
+
 				buttonStates[x][y] = imageAnalyzer.analyze(imageBlocks[x][y]);
-				System.out.println("x: " + x + ", y: " + y + " - " + imageBlocks[x][y]);
+				System.out.println("x: " + x + ", y: " + y + " - "
+						+ imageBlocks[x][y]);
 			}
 		}
 	}
 
-
-	public int getBlockWidth(){
+	public int getBlockWidth() {
 		return image.getWidth() / COLCOUNT;
 	}
 
-	public int getBlockHeight(){
+	public int getBlockHeight() {
 		return image.getHeight() / LINECOUNT;
 	}
 
-	public BufferedImage getImagePart(int x, int y){
+	public BufferedImage getImagePart(int x, int y) {
 		return imageBlocks[x][y];
 	}
 
-	public boolean getButtonState(int x, int y){
+	public boolean getButtonState(int x, int y) {
 		return buttonStates[x][y];
 	}
 
-
-
-
-
 	/**
 	 * Sets the current ImageAnalyzer
+	 * 
 	 * @param analyzer
 	 */
-	public void setFilter(IImageAnalyzer analyzer){
+	public void setFilter(IImageAnalyzer analyzer) {
 		this.imageAnalyzer = analyzer;
-		
+
 		analyzeImage();
 		notifyListeners();
 	}
 
-	public void setMatrix(int sampleCount, int loopLength){
+	public void setMatrix(int sampleCount, int loopLength) {
 		this.sampleCount = sampleCount;
 		this.loopLength = loopLength;
 
 		matrix = new int[loopLength][sampleCount];
 	}
-	
 
-
-	//defining the listener-methods
-	public void addPropertyChangeListener(String propertyName, PropertyChangeListener listener) {
+	// defining the listener-methods
+	public void addPropertyChangeListener(String propertyName,
+			PropertyChangeListener listener) {
 		changeSupport.addPropertyChangeListener(propertyName, listener);
 	}
 
@@ -159,9 +158,9 @@ public class Model implements PropertyChangeListener{
 		changeSupport.firePropertyChange(event);
 	}
 
-	//method which preserves access to this class
-	public static Model getInstance(){
-		if(instance == null){
+	// method which preserves access to this class
+	public static Model getInstance() {
+		if (instance == null) {
 			instance = new Model();
 		}
 		return instance;
@@ -171,17 +170,76 @@ public class Model implements PropertyChangeListener{
 		listeners.add(listener);
 	}
 
-	public void notifyListeners(){
+	public void notifyListeners() {
 		for (INotifyable listener : listeners) {
 			listener.update();
 		}
 	}
-	
-	
-	public boolean isFilterActive(){
+
+	public boolean isFilterActive() {
 		return true;
 	}
 
+	/**
+	 * Fill SoundManager
+	 */
+	public void buildSound(int column) {
 
+		if (column > COLCOUNT - 1) {
+			column = 0;
+		}
+
+		int count = -1;
+		sou.clear();
+
+		for (int j = 0; j < buttonStates[1].length; j++) {
+			if (buttonStates[column][j]) {
+
+				try {
+					sou.addClip("samples/" + sampleLines[j]);
+					System.out.println("sample: " + sampleLines[j] + " added");
+					count++;
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (UnsupportedAudioFileException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (LineUnavailableException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+
+		}
+
+		if (count > -1) {
+			for (int i = 0; i <= count; i++) {
+				try {
+					sou.playSound(i);
+				} catch (UnsupportedAudioFileException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (LineUnavailableException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+
+		/*
+		 * Timer timer = new Timer(500, null); timer.start();
+		 */
+
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		buildSound(++column);
+	}
 
 }
--- source/src/view/FilterSelection.java
+++ source/src/view/FilterSelection.java
@@ -8,18 +8,22 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import model.Model;
-import strategies.*;
+import strategies.ColorAnalyzer;
+import strategies.GrayScaleAnalyzer;
+import strategies.NullAnalyzer;
+import strategies.RandomAnalyzer;
+import strategies.YUVAnalyzer;
 
-
-public class FilterSelection extends JPanel{
+public class FilterSelection extends JPanel {
 
 	/*
 	 * GUI part for selecitng the filterFilterSelectionFilterSelection
 	 */
-	
+
 	Model model = Model.getInstance();
-	
+
 	// Filters
+	JButton btnPlay;
 	JButton btnNone;
 	JButton btnAll;
 	JButton btnFilterRedColorAnalyzer;
@@ -28,86 +32,92 @@ public class FilterSelection extends JPanel{
 	JButton btnFilterRandomAnalyzer;
 	JButton btnFilterGrayScaleAnalyzer;
 	JButton btnFilterYUVAnalyzer;
-	
-	public FilterSelection(){
-		
+
+	public FilterSelection() {
+
+		btnPlay = new JButton("Play");
+		btnPlay.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				model.buildSound(0);
+
+			}
+		});
+
 		btnNone = new JButton("None");
 		btnNone.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				model.setFilter(new NullAnalyzer(true));				
+				model.setFilter(new NullAnalyzer(true));
 			}
 		});
-		
+
 		btnAll = new JButton("All");
 		btnAll.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				model.setFilter(new NullAnalyzer(false));				
+				model.setFilter(new NullAnalyzer(false));
 			}
 		});
-		
-		
+
 		btnFilterRedColorAnalyzer = new JButton("Red");
 		btnFilterRedColorAnalyzer.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				model.setFilter(new ColorAnalyzer(Color.RED));				
+				model.setFilter(new ColorAnalyzer(Color.RED));
 			}
 		});
-		
+
 		btnFilterGreenColorAnalyzer = new JButton("Green");
 		btnFilterGreenColorAnalyzer.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				model.setFilter(new ColorAnalyzer(Color.GREEN));				
+				model.setFilter(new ColorAnalyzer(Color.GREEN));
 			}
 		});
-		
+
 		btnFilterBlueColorAnalyzer = new JButton("Blue");
 		btnFilterBlueColorAnalyzer.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				model.setFilter(new ColorAnalyzer(Color.BLUE));	
+				model.setFilter(new ColorAnalyzer(Color.BLUE));
 			}
 		});
-	
-		
+
 		btnFilterRandomAnalyzer = new JButton("Random");
 		btnFilterRandomAnalyzer.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				model.setFilter(new RandomAnalyzer());	
+				model.setFilter(new RandomAnalyzer());
 			}
-		});		
-		
-		
+		});
+
 		btnFilterGrayScaleAnalyzer = new JButton("Grayscale");
 		btnFilterGrayScaleAnalyzer.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				model.setFilter(new GrayScaleAnalyzer());	
+				model.setFilter(new GrayScaleAnalyzer());
 			}
 		});
-		
-		
+
 		btnFilterYUVAnalyzer = new JButton("YUV");
 		btnFilterYUVAnalyzer.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				model.setFilter(new YUVAnalyzer());	
+				model.setFilter(new YUVAnalyzer());
 			}
 		});
-		
-		
+
+		this.add(btnPlay);
 		this.add(btnNone);
 		this.add(btnAll);
 		this.add(btnFilterRedColorAnalyzer);
@@ -116,8 +126,7 @@ public class FilterSelection extends JPanel{
 		this.add(btnFilterRandomAnalyzer);
 		this.add(btnFilterGrayScaleAnalyzer);
 		this.add(btnFilterYUVAnalyzer);
-		
+
 	}
-	
 
 }
